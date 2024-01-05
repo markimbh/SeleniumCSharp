2ecc1e90b562a2f63f27632bf1355e4403bc4a63
Marcos Santana <marcos.b.santana@noesis.pt>
2024-01-05 12:39:22 -0300

    teste cadastro com sucesso

---
+++ MainProject/Test/CadastroTest.cs
@@ -0,0 +1,17 @@
+﻿namespace MainProject
+{
+    class CadastroTest : CadastroPage
+    {
+        [Test]
+        public void CadastrarUsuario()
+        {
+            LogHeader();
+            ClicarLinkCadastro();
+            DigitaNome();
+            DigitaEmail();
+            DigitaSenha();
+            ClicarBotaoCadastrar();
+            ValidarMsgSucessoCadastro();
+        }
+    }
+}
